0853817f2663860ac52c0f5481010843eb3d30cf
Josh Pitman <shidogrey@gmail.com>
2016-02-20 13:40:19 -0500

    statrtedccdc

---
+++ public/js/app.js
@@ -0,0 +1,18 @@
+  var weartherData = {};
+  $(document).ready(function(){
+    baseUrl = 'https://api.forcast.io.forcast';
+
+    $('#get-weather').on('click',showInfo);
+    function buildUrl(lat,long){
+    return baseUrl + '/' +lat+ ','+lon;
+    }
+
+    function getWeather(){
+      var lat = $
+    }
+
+
+
+
+
+  }
